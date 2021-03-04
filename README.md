d07033dd4169dcf5d27e9ea700da37ec6f58a502
ren <ren@nexuspad.com>
2021-03-04 10:50:12 -0500

    flutter upgrade 2021

--- README.md
+++ README.md
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
-# np_mobile
+# upgrades
+flutter upgrade
+sudo gem install cocoapods
+note: update Android code based on: https://flutter.dev/go/android-project-migration
 
-nexuspad mobile
-
-## Getting Started
-
-For help getting started with Flutter, view our online
-[documentation](https://flutter.io/).
-=======
-# np-mobile
->>>>>>> 6306c5f044863f40b6c3e53c0f15528b1696f368
+# simulator
+sudo xcode-select --switch /Applications/Xcode.app/Contents/Developer
+open -a Simulator
--- lib/ui/blocs/application_state_provider.dart
+++ lib/ui/blocs/application_state_provider.dart
@@ -5,12 +5,15 @@ class ApplicationStateProvider extends InheritedWidget {
   final organizeBloc = OrganizeBloc();
 
   //Take the LoginScreen Widget and push it to the InheritedWidget super class
-  ApplicationStateProvider({Key key, Widget child}) : super(key: key, child: child);
+  ApplicationStateProvider({Key key, Widget child})
+      : super(key: key, child: child);
 
   @override
   bool updateShouldNotify(_) => true;
 
   static OrganizeBloc forOrganize(BuildContext context) {
-    return (context.inheritFromWidgetOfExactType(ApplicationStateProvider) as ApplicationStateProvider).organizeBloc;
+    return (context
+            .dependOnInheritedWidgetOfExactType<ApplicationStateProvider>())
+        .organizeBloc;
   }
 }
